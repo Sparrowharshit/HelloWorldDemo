52f4de0d84164bfefb4ada1c10c56acd74ef3f30
Harshit <yoharshit98@gmail.com>
2021-10-05 13:05:23 +0530

    [Harshit Add Addition operation]

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,12 +2,21 @@
 
 namespace HelloWorld
 {
+    /// <summary>
+    /// created program for testing helloworld program
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Fellowship!");
+            Addition();
             Console.ReadLine();
+        }
+        
+        public static void Addition()
+        {
+            Console.WriteLine("Addition operation");
         }
     }
 }
